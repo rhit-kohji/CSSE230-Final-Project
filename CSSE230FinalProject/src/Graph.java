348ecb1b8ee82ec40c4de967a403dc9f297a0a96
James Koh <kohji@rose-hulman.edu>
2022-02-07 21:21:19 -0500

    added map and set to graph class

--- CSSE230FinalProject/src/Graph.java
+++ CSSE230FinalProject/src/Graph.java
@@ -1,12 +1,28 @@
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public class Graph {
+public class Graph<T extends GraphNode>{
+	
+	private Set<T> nodes;
+	private Map<String, Set<String>> edges;
+	
+	public Graph(Set<T> nodes, Map<String, Set<String>> edges) {
+		this.nodes = nodes;
+		this.edges = edges;
+	}
 
-	public class Node {
-		
+	public T getNode(String id) {
+		return nodes.stream()
+				.filter(node -> node.getID().equals(id))
+	            .findFirst()
+	            .orElseThrow(() -> new IllegalArgumentException("No node found with ID"));
 	}
 	
-	public class Edge {
-		
+	public Set<T> getEdges(T node) {
+		return edges.get(node.getID()).stream()
+	            .map(this::getNode)
+	            .collect(Collectors.toSet());
 	}
-	
 }
