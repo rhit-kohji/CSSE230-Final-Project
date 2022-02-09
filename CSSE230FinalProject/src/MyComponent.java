3915fdd6320f1de5656aa42d3ada6c4401e14ebf
rhit-fogartee <78517971+rhit-fogartee@users.noreply.github.com>
2022-02-09 10:25:46 -0500

    hopefully this doesn't mess anything up

--- CSSE230FinalProject/src/MyComponent.java
+++ CSSE230FinalProject/src/MyComponent.java
@@ -1,4 +1,21 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
 
-public class MyComponent {
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
+public class MyComponent extends JComponent {
+
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+	
+	    
+	    
+	}
 }
--- CSSE230FinalProject/src/MyViewer.java
+++ CSSE230FinalProject/src/MyViewer.java
@@ -1,9 +1,48 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class MyViewer {
-
+	static Dimension SCENE_VIEWER = new Dimension(750, 600);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			JFrame frame = new JFrame();
+
+			frame.setSize(SCENE_VIEWER);
+			frame.setTitle("230 Final Project");
+
+			frame.add(new MyComponent());
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+			
+			String[] startingLocation = {"Start"};
+	        JComboBox<String> startDropDown = new JComboBox<>(startingLocation);
+	        frame.add(startDropDown);
+	        
+//	        String[] endingLocation = {"End"};
+//	        JComboBox<String> endDropDown = new JComboBox<>(endingLocation);
+//	        frame.add(endDropDown);
+	        
+	        
+	        JRadioButton timeButton = new JRadioButton("Time");
+			JRadioButton distButton = new JRadioButton("Distance");
+			
+	        JPanel radioPanel = new JPanel(new GridLayout(0, 1));
+		    radioPanel.add(timeButton);
+		    radioPanel.add(distButton);
+		    frame.add(radioPanel, BorderLayout.LINE_START);
+			
+	 
+	        //Display the window.
+	        frame.pack();
+	        frame.setVisible(true);
 
 	}
-
 }
+
+
